5efe348368d3abcfcf8339c8bb951de5605e6f79
Salmaayache22 <salma.ayache.23@ump.ac.ma>
2025-01-25 16:20:17 +0100

    Ton message de commit

--- DHT/serializers.py
+++ DHT/serializers.py
@@ -6,8 +6,9 @@
          # fields = ['temp', 'hum'] 
 
 from rest_framework import serializers
-from .models import Dht11
+from .models import Dht11, Incident
 
+# Sérialiseur pour le modèle Dht11
 class DHT11serialize(serializers.ModelSerializer):
     temperature = serializers.FloatField(source='temp')
     humidity = serializers.FloatField(source='hum')
@@ -15,3 +16,9 @@ class DHT11serialize(serializers.ModelSerializer):
     class Meta:
         model = Dht11
         fields = ['temperature', 'humidity']
+
+# Sérialiseur pour le modèle Incident
+class IncidentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Incident
+        fields = ['timestamp', 'temperature', 'operator', 'notification_app']
